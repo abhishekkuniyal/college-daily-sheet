2ce572e0ce10db573da8397aa7bf211bc025242c
ark <abhishekkuniyal@gmail.com>
2026-04-28 11:32:23 +0530

    optimal and brute force

--- sheets/containerwithmostwater.py
+++ sheets/containerwithmostwater.py
@@ -1,4 +1,4 @@
-def container(arr):
+"""def container(arr):
    w= h = maxarea = 0
    for i in range(len(arr)):
       for j in range(i+1,len(arr)):
@@ -8,4 +8,21 @@ def container(arr):
          maxarea=  max(result,maxarea)
    return maxarea
          
-print(container([1,8,6,2,5,4,8,3,7]))
+print(container([1,8,6,2,5,4,8,3,7]))""" #brute force
+
+#optimial approach:
+"""def maxArea(self, arr: List[int]) -> int:
+              maxarea =0
+              lp =0 
+              rp = len(arr)-1
+              while lp<rp:
+                w = rp-lp
+                h = min(arr[lp],arr[rp])
+                result = w*h
+                maxarea = max(maxarea,result)
+
+                if arr[lp]<arr[rp]:
+                    lp+=1
+                else:
+                    rp-=1
+              return maxarea"""
